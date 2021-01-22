6891fec47aad189ba2ecaee1f1a3015b866be4ae
Reid Metzger <contact@reidmetzger.com>
2021-01-22 11:03:26 -0500

    disable desktop icons

---
+++ presentation-mode.py
@@ -0,0 +1,12 @@
+# get status: defaults read com.apple.finder CreateDesktop
+# set status: defaults write com.apple.finder CreateDesktop -bool TRUE; killall Finder
+import os
+
+def setDesktopIcons(value):
+    if (value == False):
+        os.system('defaults write com.apple.finder CreateDesktop -bool FALSE; killall Finder')
+    if (value == True):
+        os.system('defaults write com.apple.finder CreateDesktop -bool True; killall Finder')
+
+setDesktopIcons(True)
+
