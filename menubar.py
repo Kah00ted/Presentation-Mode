f4d0ccf1b8bc7ee5e92f66fed989d64a44d954e1
Reid Metzger <contact@reidmetzger.com>
2021-01-23 12:51:35 -0500

    Update menubar.py

--- menubar.py
+++ menubar.py
@@ -2,18 +2,29 @@ import rumps
 
 #rumps.notification(title="gofer", subtitle="gofer is starting", message='', sound=False)
 
-class goferApp(object):
-    @rumps.clicked('Presentation Mode')
-
-    def hello(sender):
-        print(f"Hello from {sender.title}")
+class menuApp(object):
 
     def __init__(self):
         self.app = rumps.App("Presentation Mode", "🧨")
+
+        self.branding = rumps.MenuItem(title="--Reid Metzger--", callback=None)
+
+        self.presentation = rumps.MenuItem(title="Presentation Mode", callback=None)
+        self.presentation.on = rumps.MenuItem(title="on", callback=self.presentationMode)
+        self.presentation.off = rumps.MenuItem(title="off", callback=self.presentationMode)
+
+        self.app.menu = [
+            self.branding, 
+            None, 
+            [self.presentation,[self.presentation.on, self.presentation.off]]
+        ]
+
+    def presentationMode(self, sender):
+        rumps.notification(title="Present", subtitle=f"Presentation Mode is {sender.title}", message='', sound=False)
 
     def run(self):
         self.app.run()
 
 if __name__ == '__main__':
-    app = goferApp()
+    app = menuApp()
     app.run()
