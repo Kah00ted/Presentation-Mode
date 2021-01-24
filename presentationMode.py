3b4c9c65829a18f2b740f6a6895323789b8a0bac
Reid Metzger <contact@reidmetzger.com>
2021-01-24 12:37:07 -0500

    add warning

--- presentationMode.py
+++ presentationMode.py
@@ -1,6 +1,20 @@
 # get status: defaults read com.apple.finder CreateDesktop
 # set status: defaults write com.apple.finder CreateDesktop -bool TRUE; killall Finder
 import os
+import tkinter
+from tkinter import messagebox
+
+def setPresentationMode(torf):
+    if (torf == True):
+        # This code is to hide the main tkinter window
+        root = tkinter.Tk()
+        root.withdraw()
+
+        # Message Box
+        finderAsk = messagebox.askokcancel("Warning", "All finder windows will be closed and some unsaved files may be deleted.", default="cancel", icon='warning')
+        if finderAsk == True:
+            print ("ok")
+        
 
 def setDesktopIcons(value):
     if (value == False):
