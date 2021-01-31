3998573c39c8e47e873739af47a96075cffee4ac
Reid Metzger <contact@reidmetzger.com>
2021-01-31 01:44:24 -0500

    update menu
    Add about tab

--- menubar.py
+++ menubar.py
@@ -4,9 +4,12 @@ from presentationMode import *
 class menuApp(object):
 
     def __init__(self):
-        self.app = rumps.App("Presentation Mode", "👀")
+        self.app = rumps.App("Presentation Mode", "👀", quit_button=None)
 
-        self.branding = rumps.MenuItem(title="--Reid Metzger--", callback=None)
+        self.about = rumps.MenuItem(title="About", callback=None)
+        self.about.by = rumps.MenuItem(title="Author: Reid Metzger", callback=None)
+        self.about.git = rumps.MenuItem(title="Github", callback=None)
+        self.about.quit = rumps.MenuItem(title="Quit App", callback=rumps.quit_application)
 
         self.presentation = rumps.MenuItem(
             title="Presentation Mode", callback=None)
@@ -17,7 +20,9 @@ class menuApp(object):
         self.app.menu = [
             #self.branding,
             #None,
-            [self.presentation, [self.presentation.on, self.presentation.off]]
+            [self.presentation, [self.presentation.on, self.presentation.off]],
+            None,
+            [self.about, [self.about.by, self.about.git, self.about.quit]]
         ]
 
     def presentationMode(self, sender):
