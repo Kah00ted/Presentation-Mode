cc3fddb99b9f48597f1cdb5da3aa519556f66079
Reid Metzger <contact@reidmetzger.com>
2021-01-30 20:21:54 -0500

    appSetup for compiling application
    probably did this wrong

---
+++ appSetup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+APP = ['menubar.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    
+    'plist': {
+        'CFBundleShortVersionString': '0.2.0',
+        'LSUIElement': True,
+    },
+    'packages': ['rumps', 'os'],
+}
+
+setup(
+    app=APP,
+    name='Presentation Mode',
+    py_modules=['presentationMode'],
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], 
+    install_requires=['rumps']
+)
