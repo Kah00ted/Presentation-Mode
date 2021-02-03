c57af649847acccd58fe9535450ae2db5a032f3c
Reid Metzger <contact@reidmetzger.com>
2021-02-02 19:55:55 -0500

    Add icon to app compiler

--- appSetup.py
+++ appSetup.py
@@ -4,7 +4,7 @@ APP = ['menubar.py']
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
-    
+    'iconfile': 'art/icon1.png',
     'plist': {
         'CFBundleShortVersionString': '0.2.0',
         'LSUIElement': True,
